d7697f1b0b0435228a8a737a1b098e877da7d329
TurtleWolfe <jonpohlner@gmail.com>
2018-09-04 10:30:53 -0400

    passing test, still need to study reg expressions
    validates, study harder

--- phoneValidator.js
+++ phoneValidator.js
@@ -1,30 +1,28 @@
-function telephoneCheck(str) {
-  // ^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
-  let numString = str;
-let numRegex = /^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/g; // Change this line
-let result = numString.match(numRegex);
-  //close, but no cigar.. 
-  if (isNaN(result)){    
+function telephoneCheck(str) {// ^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
+let numRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/g; // Change this line
+let result = str.match(numRegex);
+    console.log(str);
+    console.log(result);
+  if (result){//if result of str matching regex returns true
     console.log("true");
     return true;
   }
   else{    
-    console.log(str);
-    //  console.log(result);
     console.log("false");
     return false;
-  }
-}
-telephoneCheck("1 555-555-5555");// should return true.
-telephoneCheck("1 (555) 555-5555");// should return true.
-telephoneCheck("5555555555");// should return true.
-telephoneCheck("(555)555-5555");// should return true.
-telephoneCheck("1(555)555-5555");// should return true.
-telephoneCheck("1 555 555 5555");// should return true.
-telephoneCheck("1 456 789 4444");// should return true.
-telephoneCheck("555)-555-5555");// should return false.
-telephoneCheck("(555-555-5555");// should return false.
-//telephoneCheck("555-555-5555");// should return true.
+  }//end of false
+}//end of telephoneChecker function
+
+ // telephoneCheck("1 555-555-5555");// should return true.
+ // telephoneCheck("1 (555) 555-5555");// should return true.
+ // telephoneCheck("5555555555");// should return true.
+ // telephoneCheck("(555)555-5555");// should return true.
+ // telephoneCheck("1(555)555-5555");// should return true.
+//  telephoneCheck("1 555 555 5555");// should return true.
+  telephoneCheck("1 456 789 4444");// should return true.
+  telephoneCheck("555)-555-5555");// should return false.
+  telephoneCheck("(555-555-5555");// should return false.
+//  telephoneCheck("555-555-5555");// should return true.
 //  telephoneCheck("1 (555) 555-5555");// should return true.
 //  telephoneCheck("5555555555");// should return true.
 //  telephoneCheck("555-555-5555");// should return true.
